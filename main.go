a0371c555a617491422b32c97a64956115cec63f
Ringin Restu Pati <ringinrp21@gmail.com>
2023-05-12 04:10:33 +0700

    update

--- main.go
+++ main.go
@@ -2,38 +2,49 @@ package main
 
 import (
 	"crowdfunding/user"
-	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
 	//dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "root:@tcp(127.0.0.1:3306)/crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+
+	// fmt.Println("DB Connection Good")
+
+	// var users []user.User
+	// db.Find(&users)
+
+	// for _, user := range users {
+	// 	fmt.Println(user.Name)
+	// 	fmt.Println(user.Email)
+	// 	fmt.Println("=============")
+	// }
+
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("DB Connection Good")
-
 	var users []user.User
-	length := len(users)
-
-	fmt.Println(length)
-
 	db.Find(&users)
 
-	length = len(users)
-	fmt.Println(length)
-
-	for _, user := range users {
-		fmt.Println(user.Name)
-		fmt.Println(user.Email)
-		fmt.Println("=============")
-	}
-
+	c.JSON(http.StatusOK, users)
 }
